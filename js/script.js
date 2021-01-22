aed7a56c117083d7a791177fa4fa4f88bada0651
Ashley U <ashley.umetsu@gmail.com>
2021-01-22 13:58:05 -0600

    Added event listener for "other"
    Added an event listener that istens for "other" to be selected from the drop down menu and if
    it is then displays the "other job role?" text area

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-const userName = document.querySelector('#name').focus();;
+const userName = document.querySelector('#name').focus();
+const selectJobTitle = document.querySelector('#title');
+const otherJobRole = document.querySelector('#other-job-role');
+
+//Hides the text field to describe a job when "other" is selected by default
+
+otherJobRole.style.display = 'none';
+
+/*Listens for "other" to be selected from the drop down menu and if
+it is then displays the "other job role?" text area */
+
+selectJobTitle.addEventListener('change', (event) => {
+
+	if (event.target.value === "other") {
+		otherJobRole.style.display = 'block';
+	} else {
+		otherJobRole.style.display = 'none';
+	}
+
+});
